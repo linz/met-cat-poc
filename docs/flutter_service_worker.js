9c03a7699c5407dfb2cfff1773e3b0e474269266
jacobus <jreyneke@linz.govt.nz>
2022-03-09 23:39:05 +1300

    Github pages fix

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "14b1abca1a6cb30619c779f439fcb219",
-"/": "14b1abca1a6cb30619c779f439fcb219",
+"index.html": "e8c3e6ab39e5e34ac6ee75f5ebb3afe8",
+"/": "e8c3e6ab39e5e34ac6ee75f5ebb3afe8",
 "main.dart.js": "f73d639e2df21b2dca6abf91a73f1736",
 "manifest.json": "69f997533004147f1dc6331f9d74e087",
 "version.json": "b14c406d8366850dbbbf2ac656c66804"
